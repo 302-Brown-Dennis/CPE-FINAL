8428c46405147d34ff8cd75da16bdf25bf01a84e
1103-Rhoades-Joshua <78378206+1103-Rhoades-Joshua@users.noreply.github.com>
2022-12-05 16:16:48 -0800

    Updated functions
    Updated functions

--- sketch_Fan Motor(2).ino
+++ sketch_Fan Motor(2).ino
@@ -7,28 +7,28 @@ volatile unsigned char* port_e  = (unsigned char*) 0x2E;
 
 void setup() {
 //---set pin direction
-set_PB_as_output(5); //enable
-set_PB_as_output(4); //DIRB
-set_PB_as_output(3); //DIRA
+set_PE_as_output(5); //enable
+set_PEas_output(4); //DIRB
+set_PE_as_output(3); //DIRA
 Serial.begin(9600);
 }
 void loop() {
 //---back and forth example
 Serial.println("One way, then reverse");
-write_pb(5, 1); // enable on
+write_pe(5, 1); // enable on
 for (i=0;i<5;i++) {
-write_pb(3, 1); //one way
-write_pb(4, 0);
+write_pe(3, 1); //one way
+write_pe(4, 0);
 delay(500);
-write_pb(3, 0); //reverse
-write_pb(4, 1);
+write_pe(3, 0); //reverse
+write_pe(4, 1);
 delay(500);
 }
-write_pb(5, 1); // disable
+write_pe(5, 1); // disable
 delay(2000);
 Serial.println("fast Slow example");
 }
-void set_PB_as_output(unsigned char pin_num)
+void set_PE_as_output(unsigned char pin_num)
 {
     *ddr_e |= 0x01 << pin_num;
 }
