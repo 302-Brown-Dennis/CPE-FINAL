4d0135daff39ba48fd2e07c5781cc9a80f36dfef
1103-Rhoades-Joshua <78378206+1103-Rhoades-Joshua@users.noreply.github.com>
2022-12-05 16:36:37 -0800

    Updated fan cycle
    Updated fan cycle

--- sketch_Fan Motor(2).ino
+++ sketch_Fan Motor(2).ino
@@ -7,26 +7,27 @@ volatile unsigned char* port_e  = (unsigned char*) 0x2E;
 
 void setup() {
 //---set pin direction
-set_PE_as_output(5); //enable
-set_PEas_output(4); //DIRB
-set_PE_as_output(3); //DIRA
+set_PE_as_output(6); //enable
+set_PEas_output(5); //DIRB
+set_PE_as_output(4); //DIRA
 Serial.begin(9600);
 }
 void loop() {
 //---back and forth example
 Serial.println("One way, then reverse");
-write_pe(5, 1); // enable on
-for (i=0;i<5;i++) {
-write_pe(3, 1); //one way
-write_pe(4, 0);
+write_pe(6, 1); // enable on
+/*for (i=0;i<5;i++) {
+write_pe(4, 1); //one way
+write_pe(5, 0);
 delay(500);
-write_pe(3, 0); //reverse
-write_pe(4, 1);
+write_pe(4, 0); //reverse
+write_pe(5, 1);
 delay(500);
 }
-write_pe(5, 1); // disable
+write_pe(6, 1); // disable
 delay(2000);
 Serial.println("fast Slow example");
+*/
 }
 void set_PE_as_output(unsigned char pin_num)
 {
